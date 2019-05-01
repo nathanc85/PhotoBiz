4213d6b52c73f43de192e1a5c3fd276601dc0aa5
Nathan Costea <nathanC@knowclassic.com>
2019-05-01 10:18:37 -0400

    Sessions-list components gets its own html file.

--- src/app/sessions/sessions-list.component.ts
+++ src/app/sessions/sessions-list.component.ts
@@ -1,14 +1,21 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector : 'sessions-list',
-  template : 
-  `
-    <div>
-      <h1>Scheduled Photosessions</h1>
-    </div>
-  `
+  selector: 'sessions-list',
+  templateUrl: 'sessions-list.component.html'
 })
 export class SesssionsListComponent {
-
+  sessions = {
+    id: 1,
+    name: 'The Smiths',
+    date: '02/05/2019',
+    time: '10:00AM',
+    price: 300,
+    imageUrl: '/assets/images/angularconnect-shield.png',
+    location: {
+      address: '123 Main St.',
+      city: 'Charlotte',
+      country: 'US'
+    }
+  };
 }
