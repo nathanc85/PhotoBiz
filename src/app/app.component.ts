545cb15a37655a6faa137bfd1df8027a2fdd1e2a
Nathan Costea <nathanC@knowclassic.com>
2019-04-30 22:39:22 -0400

    Introduced the sessions-list component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   template: `
     <h2>Hello World</h2>
     <img src="assets/images/basic-shield.png"/>
+    <sessions-list></sessions-list>
   `
 })
 export class AppComponent {
---
+++ src/app/sessions/sessions-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector : 'sessions-list',
+  template : 
+  `
+    <div>
+      <h1>Scheduled Photosessions</h1>
+    </div>
+  `
+})
+export class SesssionsListComponent {
+
+}
