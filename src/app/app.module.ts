756ec5fc40b4109711b10624118ab55d3a1f074d
Nathan Costea <cnlnat@yahoo.com>
2019-05-03 10:16:42 -0400

    Using a Service to store and call photo sessions data.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { SesssionsListComponent } from './sessions/sessions-list.component';
 import { SessionThumbnailComponent } from './sessions/session-thumbnail.component';
 import { NavbarComponent} from './nav/navbar.component';
+import { SessionService } from './sessions/shared/session.service';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { NavbarComponent} from './nav/navbar.component';
     SessionThumbnailComponent,
     NavbarComponent
   ],
+  providers: [SessionService],
   imports: [
     BrowserModule
   ],
